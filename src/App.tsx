4e2654662524f915ac4e8fd53afac5c78b890af3
Ardit Nurcaj <arditnurcaj@gmail.com>
2023-01-23 23:06:02 +0100

    Cleanup app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import { useAppStore } from '@/stores';
-
-function App(): JSX.Element {
-  const { count, increase, reset } = useAppStore();
-
-  return (
-    <div>
-      <button data-testid='add-btn' onClick={() => increase()}>
-        Add
-      </button>
-      <button data-testid='reset-btn' onClick={() => reset()}>
-        Reset
-      </button>
-      <div data-testid='count'>Count: {count}</div>
-    </div>
-  );
-}
+const App = (): JSX.Element => {
+  return <div>Hello Space X</div>;
+};
 
 export default App;
